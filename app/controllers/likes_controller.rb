9d837d80756a231d70bae6ef20b287896fba6c71
omarsalem7 <omarsalem0721@gmail.com>
2022-05-12 01:24:55 +0200

    fix linetr errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,21 @@
 class LikesController < ApplicationController
-    def new
-        @new_like = Like.new
-      end
-    
-      def create
-        @post = Post.find(params[:post_id])
-        @user = current_user
-        @new_like = Like.new
-        @new_like.user = @user
-        @new_like.post = @post
+  def new
+    @new_like = Like.new
+  end
 
-    
-        if @new_like.save
-            @new_like.update_like_counter
-          redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
-        else
-          redirect_to user_post_path(user_id: @post.author_id, id: @post.id),
-                      alert: 'An error occurred while creating the like'
-        end
-      end
+  def create
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    @new_like = Like.new
+    @new_like.user = @user
+    @new_like.post = @post
+
+    if @new_like.save
+      @new_like.update_like_counter
+      redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+    else
+      redirect_to user_post_path(user_id: @post.author_id, id: @post.id),
+                  alert: 'An error occurred while creating the like'
+    end
+  end
 end
