a420b7279be3cd38031f625aed8abd28fd6d3243
omarsalem7 <omarsalem0721@gmail.com>
2022-05-12 16:22:21 +0200

    Add test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Like.new(post_id: Post.new(user: User.new(name: 'omar', photo: 'imagephoto', bio: 'software engineer'), title: 'My title', text: 'My hello world test').id , user_id:  User.new(name: 'omar', photo: 'imagephoto', bio: 'software engineer').id) }
+  before { subject.save }
+
+  it 'if post_id is integer' do
+    subject.user_id = "ss"
+    expect(subject).to_not be_valid
+  end
+  it 'if post_id is integer' do
+    subject.post_id = "123"
+    expect(subject).to_not be_valid
+  end
 end
