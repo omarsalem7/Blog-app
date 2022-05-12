30a11072ec5844a16533cbada287761da2c9fc25
omarsalem7 <omarsalem0721@gmail.com>
2022-05-12 16:08:53 +0200

    Add unit test fr post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Post.new(user: User.new(name: 'omar', photo: 'imagephoto', bio: 'software engineer'), title: 'My title', text: 'My hello world test') }
+  before { subject.save }
+
+
+  it 'validate post' do
+    expect(subject).to be_valid
+  end
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'if comments counter is float' do
+    subject.comments_counter = 0.25
+    expect(subject).not_to be_valid
+  end
+
+  it 'post counter must be an integer' do
+    subject.comments_counter = 'hello'
+    expect(subject).to_not be_valid
+  end
+
+  it 'post counter must 0 or greater than not negative numbers' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
 end
