9b94f8c2f6ec131067b36c535637a245fe9f4dff
omarsalem7 <omarsalem0721@gmail.com>
2022-05-12 16:00:52 +0200

    fix linters errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'omar',photo: 'imagephoto',bio:'software engineer')}
-  before {subject.save}
+  subject { User.new(name: 'omar', photo: 'imagephoto', bio: 'software engineer') }
+  before { subject.save }
 
-  it "name should be present" do
-    subject.name=nil
+  it 'name should be present' do
+    subject.name = nil
     expect(subject).to_not be_valid
   end
 
